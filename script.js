c807b7bf50b9b4b88ce0945a7edc29ef07816d5e
Arman Hossain <armaanepiicfb17@gmail.com>
2024-12-07 21:26:09 +0600

    completed

--- script.js
+++ script.js
@@ -17,12 +17,20 @@ const addTask = () => {
 }
 
 listContainer.addEventListener('click', function (e) {
-    if (e.target.tagName === 'LI')
+    if (e.target.tagName === 'LI') {
         e.target.classList.toggle("checked");
-    else if (e.target.tagName === "SPAN")
+        saveData();
+    }
+    else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
+        saveData();
+    }
 }, false);
 
-const saveData = () => {
+function saveData() {
     localStorage.setItem("data", listContainer.innerHTML);
 }
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
